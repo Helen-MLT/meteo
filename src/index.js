d34fcb15683201196562721c1e9a62244e351e3c
Helen-MLT <h.milletti@gmail.com>
2024-07-31 16:34:42 +0200

    Injected forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperatures">
+                <strong>15°</strong>
+                <div class="weather-forecast-temperatures">9°</div>
+              </div>
+            </div>
+          `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Brussels");
+displayForecast();
